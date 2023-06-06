f317c1b2e9089ad8281100022924db7340165c6c
Dylan Kane <dylmkane@gmail.com>
2023-06-05 20:37:40 -0400

    builder.io connected

---
+++ src/pages/[[...page]].tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+import { BuilderComponent, builder, useIsPreviewing } from '@builder.io/react';
+import DefaultErrorPage from 'next/error';
+import Head from 'next/head';
+
+// Replace with your Public API Key
+builder.init("6fe2db1df8404738be3cd2529a2b6bbe");
+
+export async function getStaticProps({ params }: any) {
+  // Fetch the builder content
+  const page = await builder
+    .get('page', {
+      userAttributes: {
+        urlPath: '/' + (params?.page?.join('/') || ''),
+      },
+    })
+    .toPromise();
+
+  return {
+    props: {
+      page: page || null,
+    },
+    revalidate: 5
+  };
+}
+
+export async function getStaticPaths() {
+  // Get a list of all pages in builder
+  const pages = await builder.getAll('page', {
+    // We only need the URL field
+    fields: 'data.url', 
+    options: { noTargeting: true },
+  });
+
+  return {
+    paths: pages.map(page => `${page.data?.url}`),
+    fallback: true,
+  };
+}
+
+export default function Page({ page }: any) {
+  const router = useRouter();
+  const isPreviewing = useIsPreviewing();
+
+  if (router.isFallback) {
+    return <h1>Loading...</h1>
+  }
+
+  if (!page && !isPreviewing) {
+    return <DefaultErrorPage statusCode={404} />
+  }
+
+  return (
+    <>
+      <Head>
+        <title>{page?.data.title}</title>
+      </Head>
+      {/* Render the Builder page */}
+      <BuilderComponent model="page" content={page} />
+    </>
+  );
+}
